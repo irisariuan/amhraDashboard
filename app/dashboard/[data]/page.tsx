4a9f54b840954cf0c356bc4446cac8ed47983552
ariuan <kms12836@gmail.com>
2024-03-24 17:06:54 +0800

    Refactor VisitorPage component to use useState and useEffect hooks

--- app/dashboard/[data]/page.tsx
+++ app/dashboard/[data]/page.tsx
@@ -10,45 +10,56 @@ enum Loading {
 	Error,
 }
 
+interface VisitorPageData {
+	token: null | string
+	gid: null | string
+}
+
 export default function VisitorPage({ params }: { params: { data: string } }) {
 	const [ok, setOk] = useState(Loading.Loading)
+	const [{ token, gid }, setData] = useState<VisitorPageData>({
+		token: null,
+		gid: null,
+	})
 	const router = useRouter()
 	useEffect(() => {
+		try {
+			const { guildId, auth }: { guildId: string; auth: string } = JSON.parse(
+				atob(decodeURI(params.data))
+			)
+
+			verifyVisitorWeb(auth, guildId)
+				.then(v => {
+					setOk(v ? Loading.Loaded : Loading.Error)
+					setData({
+						gid: guildId,
+						token: auth,
+					})
+				})
+				.catch(e => {
+					setOk(Loading.Error)
+				})
+		} catch {
+			setOk(Loading.Error)
+		}
 		if (ok === Loading.Error) {
 			router.push('/')
 		}
 	}, [ok])
 
-	try {
-		const { guildId, auth }: { guildId: string; auth: string } = JSON.parse(
-			atob(decodeURI(params.data))
-		)
-
-		verifyVisitorWeb(auth, guildId)
-			.then(v => {
-				setOk(v ? Loading.Loaded : Loading.Error)
-			})
-			.catch(e => {
-				setOk(Loading.Error)
-			})
-
-		return ok === Loading.Loaded ? (
-			<div className="flex justify-center items-center w-full h-full">
-				<div className="bg-white p-8 rounded-xl h-1/2 w-1/2">
-					<SongDashboard auth={auth} guildId={guildId} visitor={true} />
-				</div>
+	return ok === Loading.Loaded && token && gid ? (
+		<div className="flex justify-center items-center w-full h-full">
+			<div className="bg-white p-8 rounded-xl h-1/2 w-1/2">
+				<SongDashboard auth={token} guildId={gid} visitor={true} />
 			</div>
-		) : ok === Loading.Loading ? (
-			<div className="flex justify-center items-center w-full h-full">
-				<p className="text-3xl text-white">Opening...</p>
-			</div>
-		) : (
-			<div className="flex justify-center items-center w-full h-full">
-				<p className="text-3xl text-white">Error!</p>
-			</div>
-		)
-	} catch {
-		setOk(Loading.Error)
-		return <></>
-	}
+		</div>
+	) : ok === Loading.Loading ? (
+		<div className="flex justify-center items-center w-full h-full">
+			<p className="text-3xl text-white">Opening...</p>
+		</div>
+	) : (
+		<div className="flex justify-center items-center w-full h-full">
+			<p className="text-3xl text-white">Error!</p>
+		</div>
+	)
 }
