68fbaedec68f52f5cbd0cf2bf73ac2a1fefb3e2c
ariuan <kms12836@gmail.com>
2024-04-05 00:56:39 +0800

    bug fix

--- components/custom/dashboard/songDashboard.tsx
+++ components/custom/dashboard/songDashboard.tsx
@@ -102,7 +102,7 @@ export function SongDashboard({
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			if (data?.song) {
+			if (data?.song && !data.paused) {
 				setTime((Date.now() - data.song.startTime) / 1000)
 			} else {
 				setTime(0)
