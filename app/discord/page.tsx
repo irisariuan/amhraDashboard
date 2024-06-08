592ccc7962e7de1ff0c1f696d86d7678668a5b4c
ariuan <kms12836@gmail.com>
2024-06-09 01:47:18 +0800

    chore: Update DiscordRedirect to use NEXT_PUBLIC_INVITE_LINK

--- app/discord/page.tsx
+++ app/discord/page.tsx
@@ -2,13 +2,16 @@
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
+const INVITE_LINK = process.env.NEXT_PUBLIC_INVITE_LINK
+
 export default function DiscordRedirect() {
 	const router = useRouter()
 	useEffect(() => {
         if (window.localStorage.getItem("bearer")) {
             return router.push("/dashboard")
         }
-        router.push(process.env.INVITE_LINK ?? '/')
-	}, [])
+        router.push(INVITE_LINK ?? '/')
+        }, [])
+    console.log(INVITE_LINK)
     return <></>
 }
