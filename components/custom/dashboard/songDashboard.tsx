4c006d59926b1bb5d9177861511154ad05353834
irisariuan <kms12836@gmail.com>
2025-08-12 20:53:35 +0800

    Add skip non-music feature to song dashboard

--- components/custom/dashboard/songDashboard.tsx
+++ components/custom/dashboard/songDashboard.tsx
@@ -1,18 +1,18 @@
-'use client'
-import { Label } from '@/components/ui/label'
+"use client";
+import { Label } from "@/components/ui/label";
 import {
 	FormatSongEditType,
 	SongEditType,
 	YoutubeVideoRegex,
-} from '@/lib/api/song'
-import { mutate } from 'swr'
-import Area from '../area'
-import { Button } from '@/components/ui/button'
-import { editAction, queryYoutube } from '@/lib/api/web'
-import { Input } from '@/components/ui/input'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { toast } from 'sonner'
+} from "@/lib/api/song";
+import { mutate } from "swr";
+import Area from "../area";
+import { Button } from "@/components/ui/button";
+import { editAction, queryYoutube } from "@/lib/api/web";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
 import {
 	FormField,
 	FormItem,
@@ -21,110 +21,153 @@ import {
 	FormDescription,
 	FormMessage,
 	Form,
-} from '@/components/ui/form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useEffect, useState } from 'react'
-import Queue from './queue'
-import { useSongReply } from './useSongReply'
-import { ExitIcon, PauseIcon, PlusCircledIcon, ResumeIcon, StopIcon, TrackNextIcon, SpeakerOffIcon, SpeakerLoudIcon, LoopIcon, ReloadIcon } from '@radix-ui/react-icons'
-import Query from './query'
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
-import HistoryItem from '../historyItem'
-import PlaybackControl from '../ui/playbackControl'
-import DashboardPlaceholder from './dashboardPlaceholder'
-import Controllable from '../ui/controllableBar'
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import Queue from "./queue";
+import { useSongReply } from "./useSongReply";
+import {
+	ExitIcon,
+	PauseIcon,
+	PlusCircledIcon,
+	ResumeIcon,
+	StopIcon,
+	TrackNextIcon,
+	SpeakerOffIcon,
+	SpeakerLoudIcon,
+	LoopIcon,
+	ReloadIcon,
+	DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import Query from "./query";
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
+import HistoryItem from "../historyItem";
+import PlaybackControl from "../ui/playbackControl";
+import DashboardPlaceholder from "./dashboardPlaceholder";
+import Controllable from "../ui/controllableBar";
 
 const formSchema = z.object({
 	url: z.string().min(1),
-})
+});
 
 export function SongDashboard({
 	auth,
 	guildId,
 	visitor = false,
-	bearer = false
+	bearer = false,
 }: {
-	auth: string
-	guildId: string
-	visitor: boolean
-	bearer?: boolean
+	auth: string;
+	guildId: string;
+	visitor: boolean;
+	bearer?: boolean;
 }) {
-	const [volume, setVolume] = useState(0)
-	const [time, setTime] = useState<number | null>(null)
-	const [waited, setWaited] = useState(false)
-	const { data, isLoading } = useSongReply({ guildId, auth, visitor, bearer })
-	const authData = { auth, bearer, guildId, visitor }
+	const [volume, setVolume] = useState(0);
+	const [time, setTime] = useState<number | null>(null);
+	const [waited, setWaited] = useState(false);
+	const { data, isLoading } = useSongReply({
+		guildId,
+		auth,
+		visitor,
+		bearer,
+	});
+	const authData = { auth, bearer, guildId, visitor };
 	async function handleClick(action: SongEditType) {
 		if (await editAction(action, authData)) {
-			toast(FormatSongEditType[action])
+			toast(FormatSongEditType[action]);
 		} else {
-			toast('Failed to run')
+			toast("Failed to run");
 		}
-		await mutate(`/api/song/get/${guildId}`)
+		await mutate(`/api/song/get/${guildId}`);
 	}
 	async function onSubmit(values: z.infer<typeof formSchema>) {
-		let url = values.url
+		let url = values.url;
 		if (!YoutubeVideoRegex.test(values.url)) {
-			const query = await queryYoutube(values.url, authData)
-			url = query.url
-			toast(`Found song ${query.title}`)
+			const query = await queryYoutube(values.url, authData);
+			url = query.url;
+			toast(`Found song ${query.title}`);
 		}
 		if (await editAction(SongEditType.AddSong, authData, url)) {
-			mutate(`/api/song/get/${guildId}`)
-			return toast('Added song to queue')
+			mutate(`/api/song/get/${guildId}`);
+			return toast("Added song to queue");
 		}
-		return toast('Failed to add to queue')
+		return toast("Failed to add to queue");
 	}
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			url: '',
+			url: "",
 		},
-	})
+	});
 
 	useEffect(() => {
 		const id = setTimeout(() => {
-			console.log('Waiting too long!')
-			setWaited(true)
-		}, 2000)
+			console.log("Waiting too long!");
+			setWaited(true);
+		}, 2000);
 		return () => {
-			clearTimeout(id)
-		}
-	}, [])
+			clearTimeout(id);
+		};
+	}, []);
 
 	useEffect(() => {
 		if (!data) {
-			return setVolume(-1)
+			return setVolume(-1);
 		}
-		setVolume(data.volume * 100)
-
-
-	}, [data])
+		setVolume(data.volume * 100);
+	}, [data]);
 
 	useEffect(() => {
-		if (!data) return
+		if (!data) return;
 
 		if (data.song) {
 			if (data.paused) {
-				setTime((data.pausedTimestamp - data.song.startTime - data.pausedInMs + data.song.startFrom) / 1000)
+				setTime(
+					(data.pausedTimestamp -
+						data.song.startTime -
+						data.pausedInMs +
+						data.song.startFrom) /
+						1000,
+				);
 			} else {
-				setTime((Date.now() - data.song.startTime - data.pausedInMs + data.song.startFrom) / 1000)
+				setTime(
+					(Date.now() -
+						data.song.startTime -
+						data.pausedInMs +
+						data.song.startFrom) /
+						1000,
+				);
 			}
 		}
 
 		const intervalId = setInterval(() => {
-			if (!data.song) return setTime(0)
+			if (!data.song) return setTime(0);
 			if (data.paused) {
-				return setTime((data.pausedTimestamp - data.song.startTime - data.pausedInMs + data.song.startFrom) / 1000)
+				return setTime(
+					(data.pausedTimestamp -
+						data.song.startTime -
+						data.pausedInMs +
+						data.song.startFrom) /
+						1000,
+				);
 			}
-			setTime((Date.now() - data.song.startTime - data.pausedInMs + data.song.startFrom) / 1000)
-		}, 100)
+			setTime(
+				(Date.now() -
+					data.song.startTime -
+					data.pausedInMs +
+					data.song.startFrom) /
+					1000,
+			);
+		}, 100);
 		return () => {
-			clearInterval(intervalId)
-		}
-
-	}, [data, data?.pausedInMs])
+			clearInterval(intervalId);
+		};
+	}, [data, data?.pausedInMs]);
 
 	return (
 		<>
@@ -133,7 +176,9 @@ export function SongDashboard({
 			) : (
 				<div className="mt-10 gap-4 flex flex-col">
 					<div className="flex flex-col gap-4">
-						<Label className="text-3xl font-semibold">Song Dashboard</Label>
+						<Label className="text-3xl font-semibold">
+							Song Dashboard
+						</Label>
 						<div>
 							<Form {...form}>
 								<form onSubmit={form.handleSubmit(onSubmit)}>
@@ -144,16 +189,19 @@ export function SongDashboard({
 												name="url"
 												render={({ field }) => (
 													<FormItem>
-														<FormLabel>URL</FormLabel>
+														<FormLabel>
+															URL
+														</FormLabel>
 														<FormControl>
 															<Input
 																placeholder="YouTube URL or query items"
-																className='text-base placeholder:text-sm'
+																className="text-base placeholder:text-sm"
 																{...field}
 															/>
 														</FormControl>
 														<FormDescription>
-															YouTube URL to be added to queue
+															YouTube URL to be
+															added to queue
 														</FormDescription>
 														<FormMessage />
 													</FormItem>
@@ -167,96 +215,135 @@ export function SongDashboard({
 								</form>
 							</Form>
 						</div>
-						<div className="flex flex-col gap-2">
-							<div className="flex gap-2 items-center justify-center w-full *:w-full md:*:w-auto">
-								{data.song && <>
-									{
-										data.paused ? (
+						<div className="flex flex-col gap-2 justify-center items-center">
+							<div className="flex gap-2 items-center justify-between max-w-full w-max flex-wrap md:*:w-full md:flex-nowrap">
+								{data.song && (
+									<>
+										{data.paused ? (
 											<Button
 												onClick={() => {
-													handleClick(SongEditType.Resume)
+													handleClick(
+														SongEditType.Resume,
+													);
 												}}
 											>
 												<ResumeIcon />
 											</Button>
-
 										) : (
 											<Button
 												onClick={() => {
-													handleClick(SongEditType.Pause)
+													handleClick(
+														SongEditType.Pause,
+													);
 												}}
 											>
 												<PauseIcon />
 											</Button>
-										)
-									}
-									<Button
-										onClick={() => {
-											handleClick(SongEditType.Skip)
-										}}
-									>
-										<TrackNextIcon />
-									</Button>
-								</>}
+										)}
+										<Button
+											onClick={() => {
+												handleClick(SongEditType.Skip);
+											}}
+										>
+											<TrackNextIcon />
+										</Button>
+									</>
+								)}
 								<Button
 									onClick={async () => {
-										if (await editAction(SongEditType.Loop, authData, !data.loop)) {
-											toast(FormatSongEditType[SongEditType.Loop])
+										if (
+											await editAction(
+												SongEditType.Loop,
+												authData,
+												!data.loop,
+											)
+										) {
+											toast(
+												FormatSongEditType[
+													SongEditType.Loop
+												],
+											);
 										} else {
-											toast('Failed to run')
+											toast("Failed to run");
 										}
-										await mutate(`/api/song/get/${guildId}`)
+										await mutate(
+											`/api/song/get/${guildId}`,
+										);
 									}}
 								>
-									{data.loop ?
-										<LoopIcon /> : <ReloadIcon />
-									}
+									{data.loop ? <LoopIcon /> : <ReloadIcon />}
 								</Button>
 								<Button
 									onClick={() => {
-										handleClick(SongEditType.Stop)
+										handleClick(SongEditType.Stop);
 									}}
 								>
 									<StopIcon />
 								</Button>
 								<Button
 									onClick={() => {
-										handleClick(data.isMuting ? SongEditType.Unmute : SongEditType.Mute)
+										handleClick(
+											data.isMuting
+												? SongEditType.Unmute
+												: SongEditType.Mute,
+										);
 									}}
 								>
-									{
-										data.isMuting ? <SpeakerLoudIcon /> : <SpeakerOffIcon />
-									}
+									{data.isMuting ? (
+										<SpeakerLoudIcon />
+									) : (
+										<SpeakerOffIcon />
+									)}
 								</Button>
 								<Button
 									onClick={() => {
-										handleClick(SongEditType.Quit)
+										handleClick(SongEditType.Quit);
 									}}
 								>
 									<ExitIcon />
 								</Button>
 							</div>
-							<div className="">
+							{data.skipToTimestamp && (
+								<Button
+									onClick={() => {
+										handleClick(SongEditType.SkipSegment);
+									}}
+									className="w-full"
+								>
+									<span className="mr-2">Skip Non-Music</span>
+									<DoubleArrowRightIcon />
+								</Button>
+							)}
+							<div className="w-full">
 								<div className="flex gap-2 items-center">
 									<Label>Volume</Label>
-									{
-										data.isMuting &&
-										<SpeakerOffIcon />
-									}
+									{data.isMuting && <SpeakerOffIcon />}
 								</div>
 								<div className="flex gap-2 items-center">
-									<Controllable now={data.volume} totalValue={1} onRelease={async v => {
-										if (
-											!(await editAction(
-												SongEditType.SetVolume,
-												authData,
-												Number.parseFloat(v.toFixed(3))
-											))
-										) {
-											return toast('Failed to set volume')
+									<Controllable
+										now={data.volume}
+										totalValue={1}
+										onRelease={async (v) => {
+											if (
+												!(await editAction(
+													SongEditType.SetVolume,
+													authData,
+													Number.parseFloat(
+														v.toFixed(3),
+													),
+												))
+											) {
+												return toast(
+													"Failed to set volume",
+												);
+											}
+											mutate(`/api/song/get/${guildId}`);
+										}}
+										enabled
+										formatter={(v) =>
+											`${(v * 100).toFixed(1)}%`
 										}
-										mutate(`/api/song/get/${guildId}`)
-									}} enabled formatter={v => `${(v * 100).toFixed(1)}%`} />
+									/>
 									<Label>{volume.toFixed(1)}%</Label>
 								</div>
 							</div>
@@ -265,28 +352,62 @@ export function SongDashboard({
 					<Area title="Now Playing">
 						{data.song ? (
 							<div className="flex flex-col gap-2 w-full">
-								<Query url={data.song.link} authData={authData} />
+								<Query
+									url={data.song.link}
+									authData={authData}
+								/>
 								<div className="flex flex-col">
 									<div className="w-full">
-										<PlaybackControl now={Math.max(time ?? 0, 0)} totalTime={data.song.duration} enabled={data.canSeek} onRelease={async releaseTime => {
-											if (!(await editAction(SongEditType.SetTime, authData, Math.round(releaseTime)))) {
-												return toast('Failed to seek')
-											}
-											mutate(`/api/song/get/${guildId}`)
-										}} />
+										<PlaybackControl
+											now={Math.max(time ?? 0, 0)}
+											totalTime={data.song.duration}
+											enabled={data.canSeek}
+											onRelease={async (releaseTime) => {
+												if (
+													!(await editAction(
+														SongEditType.SetTime,
+														authData,
+														Math.round(releaseTime),
+													))
+												) {
+													return toast(
+														"Failed to seek",
+													);
+												}
+												mutate(
+													`/api/song/get/${guildId}`,
+												);
+											}}
+										/>
 									</div>
 									<Label className="text-neutral-700 dark:text-white">
-										{`${Math.floor((time ?? 0) / 60).toString().padStart(2, '0')}:${Math.floor((time ?? 0) % 60).toString().padStart(2, '0')}`}/{Math.floor(data.song.duration / 60).toString().padStart(2, '0')}:{(data.song.duration % 60).toString().padStart(2, '0')}
+										{`${Math.floor((time ?? 0) / 60)
+											.toString()
+											.padStart(2, "0")}:${Math.floor(
+											(time ?? 0) % 60,
+										)
+											.toString()
+											.padStart(2, "0")}`}
+										/
+										{Math.floor(data.song.duration / 60)
+											.toString()
+											.padStart(2, "0")}
+										:
+										{(data.song.duration % 60)
+											.toString()
+											.padStart(2, "0")}
 									</Label>
 								</div>
 							</div>
 						) : (
-							<Label className="text-zinc-500 italic">Not playing song</Label>
+							<Label className="text-zinc-500 italic">
+								Not playing song
+							</Label>
 						)}
 					</Area>
 					<div>
-						<Accordion type='single' collapsible>
-							<AccordionItem value='history'>
+						<Accordion type="single" collapsible>
+							<AccordionItem value="history">
 								<AccordionTrigger>
 									<h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
 										History
@@ -294,19 +415,30 @@ export function SongDashboard({
 								</AccordionTrigger>
 								<AccordionContent>
 									{!data.history ? (
-										<Label className="text-red-500 italic">An error occurred</Label>
+										<Label className="text-red-500 italic">
+											An error occurred
+										</Label>
 									) : data.history?.length > 0 ? (
-										<div className='flex flex-col gap-2'>
-											{Array.from(new Set(data.history)).map((v, i) => <div key={v}>
-												<HistoryItem link={v} authData={authData} />
-											</div>)}
+										<div className="flex flex-col gap-2">
+											{Array.from(
+												new Set(data.history),
+											).map((v, i) => (
+												<div key={v}>
+													<HistoryItem
+														link={v}
+														authData={authData}
+													/>
+												</div>
+											))}
 										</div>
 									) : (
-										<Label className="text-zinc-500 italic">No history</Label>
+										<Label className="text-zinc-500 italic">
+											No history
+										</Label>
 									)}
 								</AccordionContent>
 							</AccordionItem>
-							<AccordionItem value='queue'>
+							<AccordionItem value="queue">
 								<AccordionTrigger>
 									<h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
 										Queue
@@ -314,14 +446,18 @@ export function SongDashboard({
 								</AccordionTrigger>
 								<AccordionContent>
 									{!data.queue ? (
-										<Label className="text-red-500 italic">An error occurred</Label>
+										<Label className="text-red-500 italic">
+											An error occurred
+										</Label>
 									) : data.queue?.length > 0 ? (
 										<Queue
 											initQueue={data.queue}
 											authData={authData}
 										/>
 									) : (
-										<Label className="text-zinc-500 italic">No song in queue</Label>
+										<Label className="text-zinc-500 italic">
+											No song in queue
+										</Label>
 									)}
 								</AccordionContent>
 							</AccordionItem>
@@ -330,5 +466,5 @@ export function SongDashboard({
 				</div>
 			)}
 		</>
-	)
+	);
 }
--- lib/api/song.ts
+++ lib/api/song.ts
@@ -1,61 +1,65 @@
 export interface IQueueItem {
-    url: string,
-    repeating: boolean,
+	url: string;
+	repeating: boolean;
 }
 
 export interface SongReply {
-    song: {
-        link: string,
-        channel?: string,
-        duration: number,
-        title?: string,
-        thumbnails: string[],
-        startFrom: number,
-        startTime: number
-    } | null,
-    queue: IQueueItem[],
-    volume: number,
-    isPlaying: boolean,
-    history: string[],
-    useYoutubeDl: boolean,
-    paused: boolean,
-    pausedInMs: number,
-    pausedTimestamp: number,
-    canSeek: boolean,
-    isMuting: boolean,
-    loop: boolean,
+	song: {
+		link: string;
+		channel?: string;
+		duration: number;
+		title?: string;
+		thumbnails: string[];
+		startFrom: number;
+		startTime: number;
+	} | null;
+	queue: IQueueItem[];
+	volume: number;
+	isPlaying: boolean;
+	history: string[];
+	useYoutubeDl: boolean;
+	paused: boolean;
+	pausedInMs: number;
+	pausedTimestamp: number;
+	canSeek: boolean;
+	isMuting: boolean;
+	loop: boolean;
+	skipToTimestamp: number | null;
 }
 
 export enum SongEditType {
-    Pause = 'pause',
-    Resume = 'resume',
-    Skip = 'skip',
-    Stop = 'stop',
-    AddSong = 'addSong',
-    SetTime = 'setTime',
-    RemoveSong = 'removeSong',
-    SetVolume = 'setVolume',
-    SetQueue = 'setQueue',
-    Quit = 'quit',
-    Mute = 'mute',
-    Unmute = 'unmute',
-    Loop = 'loop',
+	Pause = "pause",
+	Resume = "resume",
+	Skip = "skip",
+	Stop = "stop",
+	AddSong = "addSong",
+	SetTime = "setTime",
+	RemoveSong = "removeSong",
+	SetVolume = "setVolume",
+	SetQueue = "setQueue",
+	Quit = "quit",
+	Mute = "mute",
+	Unmute = "unmute",
+	Loop = "loop",
+	SkipSegment = "skipSegment",
 }
 
 export const FormatSongEditType: Record<SongEditType, string> = {
-    [SongEditType.Pause]: 'Paused',
-    [SongEditType.Resume]: 'Resumed',
-    [SongEditType.Skip]: 'Skipped',
-    [SongEditType.Stop]: 'Stopped',
-    [SongEditType.AddSong]: 'Added Song',
-    [SongEditType.SetTime]: 'Set Time',
-    [SongEditType.RemoveSong]: 'Removed Song',
-    [SongEditType.SetVolume]: 'Set Volume',
-    [SongEditType.SetQueue]: 'Set Queue',
-    [SongEditType.Quit]: 'Quit',
-    [SongEditType.Mute]: 'Mute',
-    [SongEditType.Unmute]: 'Unmute',
-    [SongEditType.Loop]: 'Loop',
-} as const
+	[SongEditType.Pause]: "Paused",
+	[SongEditType.Resume]: "Resumed",
+	[SongEditType.Skip]: "Skipped",
+	[SongEditType.Stop]: "Stopped",
+	[SongEditType.AddSong]: "Added Song",
+	[SongEditType.SetTime]: "Set Time",
+	[SongEditType.RemoveSong]: "Removed Song",
+	[SongEditType.SetVolume]: "Set Volume",
+	[SongEditType.SetQueue]: "Set Queue",
+	[SongEditType.Quit]: "Quitted",
+	[SongEditType.Mute]: "Muted",
+	[SongEditType.Unmute]: "Unmuted",
+	[SongEditType.Loop]: "Loop Toggled",
+	[SongEditType.SkipSegment]: "Skipped Non-Music",
+} as const;
 
-export const YoutubeVideoRegex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?/
+export const YoutubeVideoRegex =
+	/^http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?$/;
